251e0995024ed5ebb2f54741a2865185ead0360f
Nikkolla <arebanikko@gmail.com>
2023-01-12 02:56:12 +0400

    some playlists and images added

--- pages/components/playlists.tsx
+++ pages/components/playlists.tsx
@@ -1,24 +1,47 @@
-
+import Image from "next/image";
+import { Inter } from "@next/font/google";
 
 export default function Playlists() {
   const Playlists = [
     {
-        name: "Playlist 1",
-        id: "1",
+      name: "Sorry )) it's too late",
+      id: "1",
+      Image: "2pac.jpg",
     },
     {
-        name: "Playlist 2",
-        id: "2",
+      name: "Diamond on The  Streets",
+      id: "2",
+      Image: "playboicarti.jpg",
     },
-]
-  
-    return (
+    {
+      name: "Consciousness Trap",
+      id: "3",
+      Image: "techno.jpg",
+    },
+    {
+        name: "",
+        id: "4",
+        Image: "heart.jpg",
+    },
+    {
+        name: "The Coming of Age",
+        id: "5",
+        Image: "blacktbilisi.jfif",
+    }
+  ];
+
+  return (
     <>
       <div>
-        <div className="grid grid-cols-4 text-white">
+        <div className="mx-32 w-auto grid grid-cols-4 gap-10 text-white">
           {Playlists.map((i) => (
-            <div key={i.id} >
+            <div key={i.id}>
+              <div className="justify-center">
+                <img src={i.Image} className="w-fit rounded-xl" />
+              </div>
+              <div className="text-center mt-2">
               {i.name}
+            </div>
             </div>
           ))}
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Inter } from '@next/font/google'
-import Sidebar from './components/sidebar'
-import Playlists from './components/playlists'
-import Songslist from './components/songslist'
-
-
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { Inter } from "@next/font/google";
+import Sidebar from "./components/sidebar";
+import Playlists from "./components/playlists";
+import Songslist from "./components/songslist";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Nickolas MusiC</title>
-        <meta name="description"/>
+        <meta name="description" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='bg-[#131314] min-h-screen h-auto'>
-        <div className='flex'>
-              <Sidebar />
-              <div className="h-full w-1/6"></div>
-          <div className='w-5/6'>
-            <div className='text-white flex justify-center '>White Text</div>
-            <div className='grid grid-cols-4'>
+      <main className="bg-[#131314] min-h-screen h-auto">
+        <div className="flex">
+          <Sidebar />
+          <div className="h-full w-1/6"></div>
+          <div className="w-5/6 mt-10">
+            <div className="text-white flex justify-center ">Whole Lotta</div>
+            <div>
               <Playlists />
             </div>
           </div>
         </div>
       </main>
     </>
-  )
+  );
 }
