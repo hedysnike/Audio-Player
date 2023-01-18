9dfa57730ad038232da3057d522be49b57626544
Nikkolla <arebanikko@gmail.com>
2023-01-18 23:55:33 +0400

    small commit

--- pages/components/sidebar.tsx
+++ pages/components/sidebar.tsx
@@ -8,7 +8,7 @@ export default function Sidebar() {
   return (
     <>
       <div className="fixed w-1/6 bg-[#19181A] min-h-screen h-full text-white">
-        <div className="flex justify-center mt-2 text-3xl">Niccolò</div>
+        <div className="flex justify-center mt-2 text-3xl">τὰ φίλτατα </div>
         <div className="text-center mt-20">
           <div className="my-9">
           </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Nickolas MusiC</title>
+        <title>τὰ φίλτατα</title>
         <meta name="description" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
