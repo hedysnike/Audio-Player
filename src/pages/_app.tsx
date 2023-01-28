97892dee24186bfe7e1f9d7327763a49ef65c767
Nikkolla <arebanikko@gmail.com>
2023-01-28 18:18:23 +0400

    added next-plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import "../styles/globals.css";
 import { AudioProvider } from "../components/hooks/useAudio";
+import PlausibleProvider from "next-plausible";
 
 export default function App({ Component, pageProps, isMobile }: any) {
   return (
-    <AudioProvider>
-      {Component.Layout ? (
-        <Component.Layout>
+    <PlausibleProvider domain="analytics.sunney.dev">
+      <AudioProvider>
+        {Component.Layout ? (
+          <Component.Layout>
+            <Component {...pageProps} />
+          </Component.Layout>
+        ) : (
           <Component {...pageProps} />
-        </Component.Layout>
-      ) : (
-        <Component {...pageProps} />
-      )}
-    </AudioProvider>
+        )}
+      </AudioProvider>
+    </PlausibleProvider>
   );
 }
