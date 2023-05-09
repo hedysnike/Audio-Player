7ef6de3b01263b48502809812cd5b04c8521783c
Nika <arebanikko@gmail.com>
2023-05-10 00:17:13 +0400

    tryy

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,10 @@
+import { MobileLayout } from "@/components/mobilecomponents/defaultcomponents/MobileLayout";
 import { login } from "@/lib/api";
 import { useUser } from "@/lib/hooks/useUser";
 import { Icon } from "@iconify/react";
 import { PasswordInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -27,48 +29,49 @@ export default function Login() {
 
   return (
     <>
+    <MobileLayout>
       <Head>
         <style type="text/css">
           {`
         .mantine-13e8zn2 {
-  color: white;
-}
-
-.mantine-PasswordInput-input :focus {
-  background-color: #1E1E1E;
-  color: white;
-}
-
-.mantine-PasswordInput-input {
-  background-color: #1E1E1E;
-  border: none
-}
-.mantine-titt6i {
-  color: white;
-}
-
-.mantine-16c6coe:hover {
-  background-color: transparent;
-}
-
-.mantine-1jdmi3h :hover {
-  color: white;
-  background: #1E1E1E;
-}
-
-.mantine-PasswordInput-wrapper :focus-within {
-  border: 1px solid #17687c;
-}
-
-.mantine-16c6coe:active {
-  background-color: transparent;
-}
-.mantine-PasswordInput-input :focus{
-  border: none;
-}
-.mantine-9zz0g4 {
-  color: white;
-}
+            color: white;
+        }
+        
+        .mantine-PasswordInput-input :focus {
+            background-color: #1E1E1E;
+            color: white;
+        }
+        
+        .mantine-PasswordInput-input {
+            background-color: #1E1E1E;
+            border: none
+        }
+        .mantine-titt6i {
+            color: white;
+        }
+        
+        .mantine-16c6coe:hover {
+            background-color: transparent;
+        }
+        
+        .mantine-1jdmi3h :hover {
+            color: white;
+            background: #1E1E1E;
+        }
+        
+        .mantine-PasswordInput-wrapper :focus-within {
+            border: 1px solid #17687c;
+        }
+        
+        .mantine-16c6coe:active {
+            background-color: transparent;
+        }
+        .mantine-PasswordInput-input :focus{
+            border: none;
+        }
+        .mantine-9zz0g4 {
+            color: white;
+        }
 .mantine-PasswordInput-input {
   border: 1px solid transparent;
 }
@@ -82,31 +85,31 @@ export default function Login() {
           </div>
           <form
             onSubmit={form.onSubmit((values) => {
-              const { username, password } = values;
-
-              login(username, password)
+                const { username, password } = values;
+                
+                login(username, password)
                 .then((res) => {
-                  if (res.ok) {
-                    return res.json();
-                  } else {
-                    return res.json().then((data) => {
-                      throw new Error(data.message);
-                    });
-                  }
+                    if (res.ok) {
+                        return res.json();
+                    } else {
+                        return res.json().then((data) => {
+                            throw new Error(data.message);
+                        });
+                    }
                 })
                 .then((res) => {
-                  if (res.user) {
-                    retry();
-                    setTimeout(() => {
-                      router.push("/profile"); 
-                    }, 150);
+                    if (res.user) {
+                        retry();
+                        setTimeout(() => {
+                            router.push("/profile"); 
+                        }, 150);
                     }
                 })
                 .catch((error) => {
-                  setErrorMessage(error.message);
+                    setErrorMessage(error.message);
                 });
             })}
-          >
+            >
             <div className="flex flex-row pt-[9px] pb-[6px]">
               <input
                 placeholder="Username"
@@ -115,7 +118,7 @@ export default function Login() {
                 required
                 {...form.getInputProps("username")}
                 className="w-[260px] rounded-[4px] bg-[#1E1E1E] p-[8px] pl-3 text-sm text-white  outline-none invalid:ring-red-500 focus:ring-1 focus:ring-[#17687c] md:w-[300px]"
-              />
+                />
             </div>
 
             <div className="flex flex-col pt-[9px] pb-[6px]">
@@ -123,9 +126,9 @@ export default function Login() {
                 placeholder="Password"
                 {...form.getInputProps("password")}
                 className="w-[260px] outline-none md:w-[300px]"
-              />
+                />
               {errorMessage && (
-                <div className="mb-4 text-sm text-red-500">
+                  <div className="mb-4 text-sm text-red-500">
                 </div>
               )}
 <div className="flex justify-end">
@@ -150,13 +153,26 @@ export default function Login() {
               <Link
                 href="/register"
                 className="my-2 hidden items-center gap-2 rounded-md bg-[#292929] p-2 px-4 text-sm text-white hover:bg-[#7B66C9] hover:text-[#151515] md:flex"
-              >
+                >
                 register
               </Link>
             </div>
           </form>
         </div>
       </div>
+                  </MobileLayout>
     </>
   );
 }
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const isMobile = context.req.headers["user-agent"]?.includes("Mobile");
+  
+    return {
+      props: {
+        isMobile,
+      },
+    };
+  };
+  
