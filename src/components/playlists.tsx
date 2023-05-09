efb825e35a828cd81605edf925c9ed8ea8cdb0b8
Nika <arebanikko@gmail.com>
2023-05-10 00:33:51 +0400

    width optimized for desktop

--- src/components/playlists.tsx
+++ src/components/playlists.tsx
@@ -5,12 +5,12 @@ export default function PlaylistDisplay() {
   return (
     <>
       <div>
-        <div className="mx-32 w-auto flex items-center flex-col lg:items-stretch lg:grid lg:grid-cols-4 gap-10 text-white">
+        <div className="mx-20 pt-8 w-auto flex items-center flex-col lg:items-stretch lg:grid lg:grid-cols-3 gap-10 text-white">
           {playlists.map((i) => (
             <div key={i.id} className="p-5 pb-4 bg-[#181818] hover:bg-[#232323]">
               <Link href="/playlist/[pid]" as={`/playlist/${i.id}`}>
                 <div className="justify-center flex">
-                  <img src={i.Image} className="rounded-xl w-52 h-52" />
+                  <img src={i.Image} className="rounded-xl w-52 h-52 xl:w-72 xl:h-72 2xl:w-96 2xl:h-96" />
                 </div>
                 <div className="text-center mt-2">{i.name}</div>
               </Link>
