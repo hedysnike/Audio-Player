bc39a6979430a4f7eb569e4d09239260128043ad
Nika <arebanikko@gmail.com>
2023-05-10 13:28:34 +0400

    plz

--- src/lib/hooks/useAudio.tsx
+++ src/lib/hooks/useAudio.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { Song } from "@/lib/types";
+import { Song } from "./useUser";
 
 const AudioContext = createContext<{
   audio: HTMLAudioElement | null;
