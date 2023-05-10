1f4168098b3f816bde4ad90330fc998e1c26f7e0
Nika <arebanikko@gmail.com>
2023-05-10 12:45:16 +0400

    tryyy

--- src/lib/hooks/useUser.tsx
+++ src/lib/hooks/useUser.tsx
@@ -1,5 +1,22 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
+export interface Song {
+  id: number;
+  name: string;
+  artist: string;
+  Image: string;
+  number: string;
+  URL: string;
+}
+
+export interface Playlist {
+  name: string;
+  subtitle?: string;
+  id: number;
+  Image: string;
+  songlist: Song[];
+  songlist2?: Song[];
+}
 
 export interface User {
   id:         string;
