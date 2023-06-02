90ca8dcdf7ed1b882f70f5ee2fddf754d0be4549
Nika <arebanikko@gmail.com>
2023-06-02 23:15:26 +0400

    readme updated

--- src/pages/api/logout.tsx
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function logout(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader("Set-Cookie", "token=; path=/; expires=Thu ");
-  res.status(200).json({ message: "Logged out successfully" });
-}
--- src/pages/api/uploadthing.ts
+++
@@ -1,9 +0,0 @@
-import { ourFileRouter } from "@/lib/db/uploadthing";
-import { createNextPageApiHandler } from "uploadthing/server";
- 
-const handler = createNextPageApiHandler({
-  router: ourFileRouter,
-});
- 
-export default handler;
-
